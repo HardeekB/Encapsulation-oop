4087d1ce7b395ee5b835616ab782591159e5be53
hardeek-simform <hardeek.b@simformsolutions.com>
2023-04-04 15:06:40 +0530

    completed the practical

---
+++ src/bankInterest.java
@@ -0,0 +1,11 @@
+public class bankInterest {
+    private int interestPercent = 6;
+
+    public int getInterestPercent() {
+        return interestPercent;
+    }
+
+    public void setInterestPercent(int interestPercent) {
+        this.interestPercent = interestPercent;
+    }
+}
